d8b1073bbf14bd66e33b91dc87c67b6e1db0745b
Mr-Fiddlez <126890289+Intermentality@users.noreply.github.com>
2024-12-16 10:51:14 -0500

    Redirecting assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/novainorion-iweb/',
+  base: '/NovaInOrion-IWEB/',
   plugins: [
     react(),
     glsl(),
